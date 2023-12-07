baf5e8e93c5c73423ddb57d89024136a98343ca1
Mohammad Fhadil <hafidsfhadil@gmail.com>
2023-12-07 09:07:31 +0800

    api

--- lib/shared/shared_value.dart
+++ lib/shared/shared_value.dart
@@ -1,6 +1,6 @@
 part of 'shared.dart';
 
-String apiKey = 'ebe6fa9bcfb8dcb2649bdff0faca3ed5';
+String apiKey = 'fffcce04ada81001cf7d511032a65eda';
 String? imageBaseURL = 'https://image.tmdb.org/t/p/';
 
 PageEvent? prevPageEvent;
